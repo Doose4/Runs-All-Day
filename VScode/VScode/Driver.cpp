a698ce0e2a2a077a81a13d668d7021be2b80c8b4
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-01-27 22:44:17 -0600

    Driver testing
    Testing different driver

--- VScode/VScode/Driver.cpp
+++ VScode/VScode/Driver.cpp
@@ -9,7 +9,10 @@ int main()
     Timer m;
 
     cout << "Starting tests..." << endl;
+    assert(m.runner(0) == 0);
+    assert(m.runner(1) == 1);
     assert(m.runner(2) == 2);
+    assert(m.runner(3) != 2);
     cout << "Your tests succeeded!" << endl;
    
     return 0;
